ac4e5010edbbab8e1a2fac3ad16ed263df024e83
Simon Knittel <hallo@simonknittel.de>
2024-11-30 11:10:40 +0100

    style: run Prettier

--- apps/app/src/shadcn/components/ui/textarea.tsx
+++ apps/app/src/shadcn/components/ui/textarea.tsx
@@ -1,6 +1,5 @@
-import * as React from "react"
-
-import { cn } from "@/shadcn/lib/utils"
+import { cn } from "@/shadcn/lib/utils";
+import * as React from "react";
 
 const Textarea = React.forwardRef<
   HTMLTextAreaElement,
@@ -10,13 +9,13 @@ const Textarea = React.forwardRef<
     <textarea
       className={cn(
         "flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-base shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        className
+        className,
       )}
       ref={ref}
       {...props}
     />
-  )
-})
-Textarea.displayName = "Textarea"
+  );
+});
+Textarea.displayName = "Textarea";
 
-export { Textarea }
+export { Textarea };
