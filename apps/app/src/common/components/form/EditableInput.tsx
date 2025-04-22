7383a8c42b7cfe8acf05f2b7144c3167ebf465bb
Simon Knittel <hallo@simonknittel.de>
2025-04-22 16:40:37 +0200

    feat: add - for empty EditableInput

--- apps/app/src/common/components/form/EditableInput.tsx
+++ apps/app/src/common/components/form/EditableInput.tsx
@@ -127,7 +127,7 @@ export const EditableInput = ({
           className="flex gap-2 items-center group text-left"
           title="Click to edit"
         >
-          {value}
+          {value || "-"}
           <FaPen className="flex-none text-sinister-red-500 group-hover:text-sinister-red-300 text-sm" />
         </button>
       )}
